531e1ff74f6ce3dfb0d10d7d8bc1f69d5e8d0163
isaacgichuhi <zacmuturi45@gmail.com>
2023-09-22 09:53:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Isaac is the champion';
+document.body.append(newHeader);
